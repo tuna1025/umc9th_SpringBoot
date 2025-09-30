120625dee2b13e37f20bbc75023138791593c9f8
tuna1025 <jo215098@gmail.com>
2025-09-30 13:48:20 +0900

    ✨feat: 스웨거 설정 추가

--- src/main/java/com/example/umc9th/domain/test/TestController.java
+++ src/main/java/com/example/umc9th/domain/test/TestController.java
@@ -1,9 +1,11 @@
 package com.example.umc9th.domain.test;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "테스트 API")
 public class TestController {
 
     @GetMapping("/test")
