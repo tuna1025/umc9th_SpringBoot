03846e5d92b526510a84324185b5500406a1e4a2
tuna1025 <jo215098@gmail.com>
2025-10-29 16:01:50 +0900

    ✨Feat: 내가 진행중, 진행 완료한 미션 모아서 보는 쿼리

--- src/main/java/com/example/umc9th/domain/member/service/query/MemberQueryServiceImpl.java
+++ src/main/java/com/example/umc9th/domain/member/service/query/MemberQueryServiceImpl.java
@@ -3,11 +3,16 @@ package com.example.umc9th.domain.member.service.query;
 
 import com.example.umc9th.domain.member.Exception.MemberErrorCode;
 import com.example.umc9th.domain.member.entity.Member;
+import com.example.umc9th.domain.member.entity.mapping.MemberMission;
+import com.example.umc9th.domain.member.repository.MemberMissionRepository;
 import com.example.umc9th.domain.member.repository.MemberRepository;
 import com.example.umc9th.global.exception.GeneralException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -15,9 +20,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberQueryServiceImpl {
 
     private final MemberRepository memberRepository;
+    private final MemberMissionRepository memberMissionRepository;
 
     public Member getMyPageInfo(Long memberId) {
         return memberRepository.findMyPageInfo(memberId)
                 .orElseThrow(() -> new GeneralException(MemberErrorCode.MEMBER_NOT_FOUND));
     }
+    public List<MemberMission> getMyMissions(Member member, Long cursorId) {
+        // 페이지 사이즈 10개 (LIMIT 10)
+        return memberMissionRepository.findMyMissionsByCursor(member, cursorId, PageRequest.of(0, 10));
+    }
+
 }
